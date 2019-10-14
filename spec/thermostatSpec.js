2a0b99d8a17296c600000858ed5cd6d06af7703a
zraso <zaira.rasool@hotmail.co.uk>
2019-10-14 17:03:54 +0100

    Thermostat starts at 20, test passing

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,13 @@
+describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  describe('temperature of thermostat', function(){
+    it('starts at 20 degrees', function() {
+      expect(thermostat.temperature).toEqual(20)
+    })
+  })
+})
